ec6012df7a4c070389d3f96ff4b4b568e2fa0ad6
nikudango <satoru_rutosa@icloud.com>
2021-09-23 23:06:22 +0900

    1st commit

--- OpenURL.py
+++ OpenURL.py
@@ -1,64 +1,48 @@
-from tkinter.constants import CASCADE, COMMAND
+import json
 import webbrowser
 import datetime
 from tkinter import messagebox
-import sys
-import subprocess  
 
-def WhatDay():#ここで曜日を数値に変換
-    Day = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
-    return Day.index(dow)
-
-def openURL(ClassTime,day_to_today_num):
-    if ClassTime[day_to_today_num] != '':
-        webbrowser.open(ClassTime[day_to_today_num])
-        return 0
-    else:
-        return 1
-
-def Error(ErrorPoint):
-    if(ErrorPoint > 0):
-        messagebox.showerror("error message", "授業が見つかりません")
-        print("It's not class now")
 n = 0
-ep = 0
-now = datetime.datetime.now()
 
-# ここに時間割を入力してください。
-#       　月 火 水 木 金
-first = ['','','','','']
-second= ['','','','','']
-third = ['','','','','']
-fourth= ['','','','','']
-fifth = ['','','','','']
-# 基礎機械学 https://us02web.zoom.us/j/83854424097?pwd=476334 or ?OTA4S0hoQ2ZXZEhPYU9DRGR4eTZPQT09
-# 確率統計学　https://classroom.google.com/c/Mzg4NzE2NzEzOTQ3?cjc=lxvrhj3
-# ディジタル電子回路 https://stream.meet.google.com/stream/1891a9ff-69e4-4abb-afb2-4cf0ccc20a56
-# 美術史 https://drive.google.com/file/d/1d87obK-rNvet1Dvx5UGtDrUc4NBdoSqr/view?usp=sharing”
-
+# 現在の時間を4桁の数字に変換
+# 例：12:40→1240
+now = datetime.datetime.now()   
 dow = now.strftime('%A')
 now_time = now.hour*100 + now.minute
 
+#現在が何時間目かを特定する関数
+def WhatNowClass(now_time):
+    if(now_time > 845 and now_time < 1050):
+        return 1
+    elif(now_time >= 1050 and now_time < 1240):
+        return 2
+    elif(now_time >= 1310 and now_time < 1510):
+        return 3
+    elif(now_time >= 1510 and now_time < 1700):
+        return 4
+    elif(now_time >= 1700 and now_time < 1850):
+        return 5
+    else:
+        return 0
+# ウイルスみたいなエラーを表示する
+# 不要なら以下の関数を消しても問題ない
+def Error():
+    messagebox.showerror("error message", "授業が見つかりません")
+    print("You don't have Class-Shedule!")
 
-n = WhatDay()
+# json読み込み
+url = open('./ClassSchedule.json','r') 
+data = json.load(url)
+# 今が何時間目なのか取得
+n = WhatNowClass(now_time)  
 
-if(now_time > 845 and now_time < 1050):
-    ep = openURL(first,n)
+# テスト
+# n = 1
+# dow = "Monday"
 
-elif(now_time >= 1050 and now_time < 1240):
-    ep = openURL(second,n)
-
-elif(now_time >= 1310 and now_time < 1510):
-    ep = openURL(third,n)
-
-elif(now_time >= 1510 and now_time < 1700):
-    ep = openURL(fourth,n)
-
-elif(now_time >= 1700 and now_time < 1850):
-    ep = openURL(fifth,n)
+if (n == 0):
+    Error()
 else:
-    ep += 1
-
-Error(ep)
-    
+    webbrowser.open(data[str(dow)][str(n)]["url"])
 
